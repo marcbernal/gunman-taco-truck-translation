ecfb0cd5d3df1f6c6255a51d4c6cd8c95431226b
Marc Bernal <marcbernal@gmail.com>
2017-01-18 10:37:45 -0600

    Noves traduccions

--- new-translations.lua
+++ new-translations.lua
@@ -1,11 +1,11 @@
 -- st_data["#screenshot-saved"]		= "Save a screenshot?"
-st_data["#screenshot-saved"]		= ""
+st_data["#screenshot-saved"]		= "Capturar pantalla?"
 
 -- st_data["#cant-save-screenshot"]	= "You need to grant access to the photo library in order for Gunman Taco Truck to save screenshots."
-st_data["#cant-save-screenshot"]	= ""
+st_data["#cant-save-screenshot"]	= "Necessites donar access a la llibreria de fotos per poder guardar una captura de pantalla."
 
 -- st_data["#new-highscore"]			= "NEW HIGHSCORE!"
-st_data["#new-highscore"]			= ""
+st_data["#new-highscore"]			= "NOVA MILLOR PUNTUACI"
 
 -- st_data["#dedication"]		= "Dedicated to main.lua"
 st_data["#dedication"]			= ""
@@ -44,3 +44,20 @@ st_data["#killed-by-in"]			= ""
 
 -- st_data["#killed-by-on"]			= "Killed by %s on the way to %s"
 st_data["#killed-by-on"]			= ""
+
+
+
+-- st_data["#full-auto"] = "All Weapons are Automatic"
+st_data["#full-auto"] = "Todas las Armas son Automáticas"
+
+-- st_data[ "#rocco-fix-truck" ] = "Your truck looks like a bomb hit it. Buy a toolkit and I'll repair it."
+st_data[ "#rocco-fix-truck" ] = "Tu camión luce como si le hubiera estallado una bomba. Compra un kit de reparación y lo arreglaré."
+
+-- st_data["#easy"] = "Easy"
+st_data["#easy"] = "Fácil"
+
+-- st_data["#normal"] = "Normal"
+st_data["#normal"] = "Normal"
+
+-- st_data["#hard"] = "Hard"
+st_data["#hard"] = "Difícil"
