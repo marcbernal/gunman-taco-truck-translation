6a7aae73ff07ef051e236be2170418370ccf654d
Marc Bernal <marcbernal@gmail.com>
2017-01-18 10:43:03 -0600

    Traducció complerta

--- new-translations.lua
+++ new-translations.lua
@@ -5,56 +5,58 @@ st_data["#screenshot-saved"]		= "Capturar pantalla?"
 st_data["#cant-save-screenshot"]	= "Necessites donar access a la llibreria de fotos per poder guardar una captura de pantalla."
 
 -- st_data["#new-highscore"]			= "NEW HIGHSCORE!"
-st_data["#new-highscore"]			= "NOVA MILLOR PUNTUACI"
+st_data["#new-highscore"]			= "NOVA MILLOR PUNTUACIÓ!"
 
 -- st_data["#dedication"]		= "Dedicated to main.lua"
-st_data["#dedication"]			= ""
+st_data["#dedication"]			= "Dedicat a main.lua"
 
 
 st_data[ "#action_tutorial_desktop" ] =
 {
 
 -- 		"Roadside signs are an excellent source of scrap!",
-		"",	
+		"Les senyals de trànsit son excel·lents per aconseguir ferralla!",	
 }
 
 -- st_data[ "#ticket_store_title" ] 		= "TOCCO'S TICKETS"
-st_data[ "#ticket_store_title" ] 			= ""
+st_data[ "#ticket_store_title" ] 			= "TIQUETS D'EN TOCCO"
 
 -- st_data[ "#customer-hello" ]				= "Hello there, taco please."
-st_data[ "#customer-hello" ]				= ""
+st_data[ "#customer-hello" ]				= "Ei! Un taco si us plau."
 
 -- st_data[ "#sales" ]						= "Sales"
-st_data[ "#sales" ]							= ""
+st_data[ "#sales" ]							= "Vendes"
 
 -- st_data[ "#tips" ]						= "Tips"
-st_data[ "#tips" ]							= ""
+st_data[ "#tips" ]							= "Propines"
 
 -- st_data[ "#total" ]						= "Total"
-st_data[ "#total" ]							= ""
+st_data[ "#total" ]							= "Total"
 
 -- st_data[ "#performance" ]				= "Performance"
-st_data[ "#performance" ]					= ""
+st_data[ "#performance" ]					= "Rendiment"
 
 -- st_data["#new-recipes"]	 		= "NEW RECIPES!"
-st_data["#new-recipes"]				= ""
+st_data["#new-recipes"]				= "NOVES RECEPTES!"
 
 -- st_data["#killed-by-in"]			= "Killed by %s in %s"
-st_data["#killed-by-in"]			= ""
+st_data["#killed-by-in"]			= "Mort per %s a %s"
 
 -- st_data["#killed-by-on"]			= "Killed by %s on the way to %s"
-st_data["#killed-by-on"]			= ""
+st_data["#killed-by-on"]			= "Mort per %s de camí a %s"
 
 
+
+-- Last sent translations
 
 -- st_data["#full-auto"] = "All Weapons are Automatic"
-st_data["#full-auto"] = "Todas las Armas son Automáticas"
+st_data["#full-auto"] = "Totes les Armes són Automàtiques"
 
 -- st_data[ "#rocco-fix-truck" ] = "Your truck looks like a bomb hit it. Buy a toolkit and I'll repair it."
-st_data[ "#rocco-fix-truck" ] = "Tu camión luce como si le hubiera estallado una bomba. Compra un kit de reparación y lo arreglaré."
+st_data[ "#rocco-fix-truck" ] = "El teu camió es veu com si li hagués explotat una bomba. Compra un caixa d'eines i l'arreglaré."
 
 -- st_data["#easy"] = "Easy"
-st_data["#easy"] = "Fácil"
+st_data["#easy"] = "Fàcil"
 
 -- st_data["#normal"] = "Normal"
 st_data["#normal"] = "Normal"
