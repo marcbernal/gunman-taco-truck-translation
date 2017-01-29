ffda64db70132be5164ee7e1ef263b049e6e9baf
Marc Bernal <marcbernal@gmail.com>
2017-01-29 16:59:09 -0600

    Added new translations

---
+++ added_translations_290117.lua
@@ -0,0 +1,41 @@
+-- st_data["#miles"] = "Miles"
+st_data["#miles"] = ""
+
+-- st_data["#controls"]			= "Controls"
+st_data["#controls"]			= "Controls"
+
+-- st_data["#controls-button"]		= "CONTROLS"
+st_data["#controls-button"]		= "CONTROLS"
+
+-- st_data["#controls-lanes"]		= "Change Lanes"
+st_data["#controls-lanes"]		= "Change Lanes"
+
+-- st_data["#controls-shoot"]		= "Fire Weapon"
+st_data["#controls-shoot"]		= "Fire Weapon"
+
+-- st_data["#controls-rocket"]		= "Fire Rocket"
+st_data["#controls-rocket"]		= "Fire Rocket"
+
+-- st_data["#controls-cancel"]		= "Cancel Order"
+st_data["#controls-cancel"]		= "Cancel Order"
+
+-- st_data["#lanes-desktop"]		= "Up or Down / W or S"
+st_data["#lanes-desktop"]		= "Up or Down / W or S"
+
+-- st_data["#lanes-mobile"]		= "Swipe truck up or down"
+st_data["#lanes-mobile"]		= "Swipe truck up or down"
+
+-- st_data["#shoot-desktop"]		= "Click in front of the truck"
+st_data["#shoot-desktop"]		= "Click in front of the truck"
+
+-- st_data["#shoot-mobile"]		= "Tap in front of the truck"
+st_data["#shoot-mobile"]		= "Tap in front of the truck"
+
+-- st_data["#rocket-desktop"]		= "Spacebar"
+st_data["#rocket-desktop"]		= "Spacebar"
+
+-- st_data["#rocket-mobile"]		= "Two finger tap"
+st_data["#rocket-mobile"]		= "Two finger tap"
+
+-- st_data["#cancel-desktop"]		= "Spacebar or Escape"
+st_data["#cancel-desktop"]		= "Spacebar"
