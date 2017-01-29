afff13ec87bbc6b0b8b473deec42fea46d0c6ad0
Marc Bernal <marcbernal@gmail.com>
2017-01-29 17:05:49 -0600

    Afegides traduccions

--- added_translations_290117.lua
+++ added_translations_290117.lua
@@ -1,5 +1,5 @@
 -- st_data["#miles"] = "Miles"
-st_data["#miles"] = ""
+st_data["#miles"] = "Milles"
 
 -- st_data["#controls"]			= "Controls"
 st_data["#controls"]			= "Controls"
@@ -8,34 +8,34 @@ st_data["#controls"]			= "Controls"
 st_data["#controls-button"]		= "CONTROLS"
 
 -- st_data["#controls-lanes"]		= "Change Lanes"
-st_data["#controls-lanes"]		= "Change Lanes"
+st_data["#controls-lanes"]		= "Canviar de Carril"
 
 -- st_data["#controls-shoot"]		= "Fire Weapon"
-st_data["#controls-shoot"]		= "Fire Weapon"
+st_data["#controls-shoot"]		= "Disparar Arma"
 
 -- st_data["#controls-rocket"]		= "Fire Rocket"
-st_data["#controls-rocket"]		= "Fire Rocket"
+st_data["#controls-rocket"]		= "Disparar Cohet"
 
 -- st_data["#controls-cancel"]		= "Cancel Order"
-st_data["#controls-cancel"]		= "Cancel Order"
+st_data["#controls-cancel"]		= "Cancel·lar Comanda"
 
 -- st_data["#lanes-desktop"]		= "Up or Down / W or S"
-st_data["#lanes-desktop"]		= "Up or Down / W or S"
+st_data["#lanes-desktop"]		= "Amunt o Avall / W o S"
 
 -- st_data["#lanes-mobile"]		= "Swipe truck up or down"
-st_data["#lanes-mobile"]		= "Swipe truck up or down"
+st_data["#lanes-mobile"]		= "Mou el camió amunt o avall"
 
 -- st_data["#shoot-desktop"]		= "Click in front of the truck"
-st_data["#shoot-desktop"]		= "Click in front of the truck"
+st_data["#shoot-desktop"]		= "Clica davant del camió"
 
 -- st_data["#shoot-mobile"]		= "Tap in front of the truck"
-st_data["#shoot-mobile"]		= "Tap in front of the truck"
+st_data["#shoot-mobile"]		= "Prem davant del camió"
 
 -- st_data["#rocket-desktop"]		= "Spacebar"
-st_data["#rocket-desktop"]		= "Spacebar"
+st_data["#rocket-desktop"]		= "Barra d'espai"
 
 -- st_data["#rocket-mobile"]		= "Two finger tap"
-st_data["#rocket-mobile"]		= "Two finger tap"
+st_data["#rocket-mobile"]		= "Prem amb dos dits"
 
 -- st_data["#cancel-desktop"]		= "Spacebar or Escape"
-st_data["#cancel-desktop"]		= "Spacebar"
+st_data["#cancel-desktop"]		= "Barra d'espai"
